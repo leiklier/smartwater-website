adb557604b83392cc845709e63908e2e7c576439
Leik Lima-Eriksen <leik.lima-eriksen@lyse.net>
2019-02-15 21:46:30 +0100

    Fix height to 100% on Main

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App" style={{ height: '100%' }}>
-				<Layout>
+				<Layout style={{ height: '100%' }}>
 					<Navigationbar />
 					<Main />
 				</Layout>
--- src/components/Dashboard/NodeView.js
+++ src/components/Dashboard/NodeView.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
+import { fetchNodes } from '../../actions/nodesActions'
+import { pushMeasurement } from '../../actions/measurementsActions'
+
+@connect(store => {
+	return {
+		nodes: store.nodes.nodes,
+		fetching: store.nodes.fetching,
+		fetched: store.nodes.fetched,
+		error: store.nodes.error,
+		measurements: store.measurements,
+		nodeId: `${store.router.location.pathname.split('/')[3]}`
+	}
+})
 class NodeView extends Component {
 	constructor(props) {
 		super(props)
 	}
+	componentWillMount() {
+		const { fetched, measurements, nodeId } = this.props
+		if (!fetched) {
+			this.props.dispatch(fetchNodes())
+		}
+		if (!measurements[nodeId]) {
+			this.props.dispatch(pushMeasurement(nodeId))
+		}
+	}
+
 	render() {
 		return <div>Hello from nodeview!</div>
 	}
